46156ee1dcd2fa0aa45ea91e7c5ad304961fe217
microcai <microcaicai@gmail.com>
2025-03-28 21:29:28 +0800

    fix ldscript

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -11,7 +11,7 @@ mcu = board.get("build.mcu", "")
 product_line = board.get("build.product_line", "")
 bsp = board.get("build.bsp", "")
 maximum_size = board.get("upload.maximum_size", "65536")
-
+maximum_ram_size = board.get("upload.maximum_ram_size", "1024")
 env.SConscript("_bare.py")
 
 FRAMEWORK_DIR = platform.get_package_dir("Arduino-PY32")
@@ -46,7 +46,8 @@ env.Append(
         join(FRAMEWORK_DIR, "system", "Arduino-PY32F0xx-Drivers", "PY32F0xx_HAL_Driver", "Inc"),
         join(FRAMEWORK_DIR, "system", "PY32F0xx"),
         join(FRAMEWORK_LIB_DIR, "Arduino", "inc"),
-        join(FRAMEWORK_DIR, "libraries", "Wire", "src")
+        join(FRAMEWORK_LIB_DIR, "Wire", "src"),
+        join(FRAMEWORK_DIR, "system", "Arduino-PY32F0xx-Drivers", "CMSIS", "Device", "PY32F0xx", "Source", "gcc"),
     ]
 )
 
@@ -58,6 +59,7 @@ env.Append(
         "VDD_3V3",
         "NO_ATOMIC",
         "USE_FULL_LL_DRIVER",
+        "HAL_TIM_MODULE_ENABLED",
         mcu.upper(),
         "NDEBUG",
     ]
@@ -71,12 +73,20 @@ env.Replace(LDSCRIPT_PATH=get_linker_script())
 
 extra_flags = board.get("build.extra_flags", "")
 
+# libs.append(env.BuildLibrary(
+#     join("$BUILD_DIR", "py32startup"),
+#     join(FRAMEWORK_DIR, "system", "Arduino-PY32F0xx-Drivers", "CMSIS", "Device", "PY32F0xx", "Source"),
+#     src_filter=[
+#         "+<*.c>",
+#         "+<gcc/startup_%s.s>" % bsp.lower()
+#     ]
+# ))
+
 libs.append(env.BuildLibrary(
     join("$BUILD_DIR", "py32startup"),
-    join(FRAMEWORK_DIR, "system", "Arduino-PY32F0xx-Drivers", "CMSIS", "Device", "PY32F0xx", "Source"),
+    join(FRAMEWORK_DIR, "cores", "arduino", "py32"),
     src_filter=[
-        "+<*.c>",
-        "+<gcc/startup_%s.s>" % bsp.lower()
+        "+<*.S>"
     ]
 ))
 
@@ -109,9 +119,8 @@ libs.append(env.BuildLibrary(
     ]
 ))
 
-
 libs.append(env.BuildLibrary(
-    join("$BUILD_DIR", "arduino", "py32ll_drivers"),
+    join("$BUILD_DIR", "arduino", "py32hal_drivers"),
     join(FRAMEWORK_DIR, "system", "Arduino-PY32F0xx-Drivers", "PY32F0xx_HAL_Driver", "Src"),
     src_filter=[
         "+<*.c>",
@@ -132,7 +141,10 @@ env.Append(LIBS=libs)
 
 env.Append(LINKFLAGS=[
     "-Wl,-Map,%s/firmware.map" % env.get("BUILD_DIR"),
-    "-Wl,--defsym=LD_MAX_DATA_SIZE=%s" % maximum_size,
-    "-Wl,--defsym=LD_FLASH_OFFSET=0"
-
+    "-Wl,--defsym=LD_MAX_SIZE=%s" % maximum_size,
+    "-Wl,--defsym=LD_MAX_DATA_SIZE=%s" % maximum_ram_size,
+    "-Wl,--defsym=LD_FLASH_OFFSET=0",
+    "-Wl,--gc-sections",
+    "-Wl,--entry=Reset_Handler",
+    "-Wl,--script=%s/ldscript.ld" % join(FRAMEWORK_DIR, "system"),
 ])
